dfcc85ce7a5dc41c2039f1e54dc4f595c82eab36
kelly <152154777+Umogi@users.noreply.github.com>
2024-11-08 22:53:03 +0200

    Update Drops.c

--- MainGame/Drops.c
+++ MainGame/Drops.c
@@ -7,9 +7,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h> 
-#include <windows.h>
 #include <unistd.h>
-#include <conio.h>
+
+#ifdef _WIN32
+    #include <windows.h>
+    #include <conio.h>
+#elif defined(__linux__)||define(__APPLE__)
+    #include <unistd.h>
+    #include <termios.h>
+    void Sleep(int ms){
+    	usleep(ms * 1000)
+    }
+
+    int _getch() {
+        struct termios oldt, newt;
+        int ch;
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        ch = getchar();
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        return ch;
+    }
+#endif
 
 void initializeDrops(){
 
